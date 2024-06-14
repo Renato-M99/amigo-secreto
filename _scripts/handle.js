4c5baf4c0ce3e8787552ea76dfca363153662d92
Renato <renatojunior7@hotmail.com>
2024-06-13 22:23:44 -0300

    ajustes na msg de erro

--- _scripts/handle.js
+++ _scripts/handle.js
@@ -4,31 +4,39 @@ let friendCount = 0;
 
 
 addFriend(); // add button
-restartFriends();
 
 
-function addFriend(){
+
+function addFriend() {
     document.querySelector(".Adicionar").addEventListener('click', (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
         let addBtn = document.querySelector(".in").value;
-        if (addBtn != '' ) {
+        if (addBtn != '') {
             friendCount++;
             let p = document.createElement('p');
             addFriends.append(p);
-            
+
             if (friendCount == 1) {
                 p.innerHTML = `${addBtn}`;
             } else {
                 p.innerHTML = `, ${addBtn}`;
             }
-           document.querySelector("input").value = '';
-            
+            document.querySelector("input").value = '';
+
         }
         else {
-            alert("vazio!");
+            let span = document.querySelector("span");
+            let p = document.createElement('p');
+
+            span.append(p);
+            p.innerHTML = "Erro, campo vazio!";
+            span.style.display = "block";
+            span.style.justifyContent = "center"
+            p.style.color = "red";
+          
         }
 
-     
+
     });
 }
 
