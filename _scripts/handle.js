bba718c68f35cab030787bf3216fd0ad6612a6e4
Renato <renatojunior7@hotmail.com>
2024-06-14 20:16:31 -0300

    Add function final

--- _scripts/handle.js
+++ _scripts/handle.js
@@ -1,8 +1,9 @@
 let addFriends = document.querySelector(".added");
-let addedFriends = document.querySelectorAll("p");
+let addedFriends =  Array.from(document.querySelectorAll("p"));
 let friendCount = 0;
-
-
+let span = document.querySelector("span");
+let retorno = Array.from(span);
+let error = document.createElement('p');
 addFriend(); // add button
 
 
@@ -10,32 +11,37 @@ addFriend(); // add button
 function addFriend() {
     document.querySelector(".Adicionar").addEventListener('click', (event) => {
         event.preventDefault();
+        
         let addBtn = document.querySelector(".in").value;
-        if (addBtn != '') {
+        
+        if (addBtn != '' && !addedFriends.includes(addBtn)) {
             friendCount++;
+            
             let p = document.createElement('p');
             addFriends.append(p);
-
+            
             if (friendCount == 1) {
                 p.innerHTML = `${addBtn}`;
             } else {
                 p.innerHTML = `, ${addBtn}`;
             }
+            addedFriends.push(addBtn);
             document.querySelector("input").value = '';
-
+            error.textContent = '';
+        }
+        else if(addedFriends.includes(addBtn)){
+            span.append(error);
+            error.innerHTML = "Já existe alguém com esse nome";
+            error.style.color = "red";
+            error.style.textAlign = "center";
         }
         else {
-            let span = document.querySelector("span");
-            let p = document.createElement('p');
-
-            span.append(p);
-            p.innerHTML = "Erro, campo vazio!";
-            span.style.display = "block";
-            span.style.justifyContent = "center"
-            p.style.color = "red";
-          
+            span.append(error);
+            error.innerHTML = "Erro, campo vazio!";
+            error.style.color = "red";
+            error.style.textAlign = "center";
         }
-
+        
 
     });
 }
