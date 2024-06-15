f1c29175234d55fe84b43c79836dd588f6d50bca
Renato <renatojunior7@hotmail.com>
2024-06-14 21:54:23 -0300

    sorteio 1

--- _scripts/handle.js
+++ _scripts/handle.js
@@ -1,41 +1,93 @@
 let addFriends = document.querySelector(".added");
-let addedFriends = document.querySelectorAll("p");
+let addedFriends = Array.from(document.querySelectorAll("p"));
+let out = document.querySelector(".out");
 let friendCount = 0;
+let span = document.querySelector("span");
+let retorno = Array.from(span);
+let error = document.createElement('p');
 
 
 addFriend(); // add button
-restartFriends(); //restart button
+restartFriends(); //reset
 
-function addFriend(){
+
+function addFriend() {
     document.querySelector(".Adicionar").addEventListener('click', (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
+
         let addBtn = document.querySelector(".in").value;
-        if (addBtn != '' ) {
+
+        if (addBtn != '' && !addedFriends.includes(addBtn)) {
             friendCount++;
             let p = document.createElement('p');
             addFriends.append(p);
-            
+
             if (friendCount == 1) {
                 p.innerHTML = `${addBtn}`;
             } else {
                 p.innerHTML = `, ${addBtn}`;
             }
-
+            addedFriends.push(addBtn);
+            document.querySelector("input").value = '';
+            error.textContent = '';
+        }
+        else if (addedFriends.includes(addBtn)) {
+            span.append(error);
+            error.innerHTML = "Já existe alguém com esse nome";
+            error.style.color = "red";
+            error.style.textAlign = "center";
+            document.querySelector("input").value = '';
         }
         else {
-            alert("vazio!");
+            span.append(error);
+            error.innerHTML = "Erro, campo vazio!";
+            error.style.color = "red";
+            error.style.textAlign = "center";
         }
 
-     
+
     });
 }
 
-function restartFriends(){
-    document.querySelector(".Reiniciar").addEventListener('click', (event) => {
-       
+
+
+
+function restartFriends() {
+    document.querySelector(".Reiniciar").addEventListener('click', () => {
+
+
+    });
+
+}
+
+function sortear() {
+    document.querySelector(".Sortear").addEventListener('click', (event) => {
+        event.preventDefault();
+        for (let i = addedFriends.length; i; i--) {
+            let randomValue = Math.floor(Math.random() * i);
+            [addedFriends[i], addedFriends[randomValue]] =
+            [addedFriends[randomValue], addedFriends[i]];
+        }
     });
 }
 
-function sortear(){
+/*  function shuffle(array) {
+  let currentIndex = array.length;
 
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
 }
+
+// Used like so
+let arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr); */
