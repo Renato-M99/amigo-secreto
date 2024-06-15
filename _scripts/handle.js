7d0b222bde2dcf7f617af037cc84c787707d5d2e
Renato <renatojunior7@hotmail.com>
2024-06-14 21:05:42 -0300

    reset

--- _scripts/handle.js
+++ _scripts/handle.js
@@ -1,37 +1,60 @@
 let addFriends = document.querySelector(".added");
-let addedFriends = document.querySelectorAll("p");
+let addedFriends = Array.from(document.querySelectorAll("p"));
 let friendCount = 0;
+let span = document.querySelector("span");
+let retorno = Array.from(span);
+let error = document.createElement('p');
 
 
 addFriend(); // add button
-restartFriends(); //restart button
+restartFriends(); //reset
 
-function addFriend(){
+
+function addFriend() {
     document.querySelector(".Adicionar").addEventListener('click', (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
+
         let addBtn = document.querySelector(".in").value;
-        if (addBtn != '' ) {
+
+        if (addBtn != '' && !addedFriends.includes(addBtn)) {
             friendCount++;
             let p = document.createElement('p');
             addFriends.append(p);
-            
+
             if (friendCount == 1) {
                 p.innerHTML = `${addBtn}`;
             } else {
                 p.innerHTML = `, ${addBtn}`;
             }
-
+            addedFriends.push(addBtn);
+            document.querySelector("input").value = '';
+            error.textContent = '';
+        }
+        else if (addedFriends.includes(addBtn)) {
+            span.append(error);
+            error.innerHTML = "Já existe alguém com esse nome";
+            error.style.color = "red";
+            error.style.textAlign = "center";
+            document.querySelector("input").value = '';
         }
         else {
-            alert("vazio!");
+            span.append(error);
+            error.innerHTML = "Erro, campo vazio!";
+            error.style.color = "red";
+            error.style.textAlign = "center";
         }
 
-     
+
     });
 }
 
-function restartFriends(){
-    document.querySelector(".Reiniciar").addEventListener('click', (event) => {
+
+
+
+function restartFriends() {
+    document.querySelector(".Reiniciar").addEventListener('click', () => {
+        
        
     });
+ 
 }
